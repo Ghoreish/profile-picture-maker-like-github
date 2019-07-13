c07fe20fbf4779eba8b61ba8ffafcf82c0c03e67
ghoreish <sghoreish@gmail.com>
2019-07-13 10:01:43 +0430

    improved

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import random
 def make():
     l=[]
-    for i in range(10):
+    for i in range(9):
         l.append("")
         for i in range(5):
             l[-1]+= "⬜" if random.randint(0,1) == 1 else "⬛"
 
     for i in l:
-        print(i+i[::-1])
+        print(i+i[-2::-1])
 
 make()
